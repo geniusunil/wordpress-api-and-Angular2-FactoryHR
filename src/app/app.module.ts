c9327f15f15f23287a5a3d8a714cc46b7b324d27
geniusunil <geniusunil@gmail.com>
2019-03-12 17:11:43 +0530

    working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,42 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CategoryComponent } from './category/category.component';
+import { SingleComponent } from './single/single.component';
+import { RouterModule, Routes } from '@angular/router';
+import { PostsService } from './services/posts.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
+const appRoutes: Routes = [
+  {
+    path: ':slug',
+    component: SingleComponent
+  },
+  { path: '',
+    component: CategoryComponent,
+    pathMatch: 'full'
+  }
+ ];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CategoryComponent,
+    SingleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    HttpClientModule
   ],
-  providers: [],
+  providers: [PostsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
