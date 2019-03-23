8b1218f5ceef3c960dcf98ca012cd592e5d4ac2c
geniusunil <geniusunil@gmail.com>
2019-03-23 12:15:55 +0530

    conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,42 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { CategoryComponent } from './category/category.component';
-import { SingleComponent } from './single/single.component';
-import { RouterModule, Routes } from '@angular/router';
-import { PostsService } from './services/posts.service';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 
-const appRoutes: Routes = [
-  {
-    path: ':slug',
-    component: SingleComponent
-  },
-  { path: '',
-    component: CategoryComponent,
-    pathMatch: 'full'
-  }
- ];
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    CategoryComponent,
-    SingleComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
-    ),
-    HttpClientModule
+    AppRoutingModule
   ],
-  providers: [PostsService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
